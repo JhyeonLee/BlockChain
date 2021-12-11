db35f40c15b40bba4934ef5a4fdb50c26611f00c
JhyeonLee <jhyeonlee98@gmail.com>
2021-12-11 10:27:39 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JhyeonLee/BlockChain
+
+go 1.13
---
+++ person/person.go
@@ -0,0 +1,19 @@
+package person
+
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+}
+
+func (p *Person) SetDetails(name string, age int) {
+	p.name = name
+	p.age = age
+
+	fmt.Println(p.name, p.age)
+}
+
+func (p Person) Name() string {
+	return p.name
+}
